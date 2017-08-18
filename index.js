72dee14de24c680d5b74460c7433b62515fdc355
ELTORO.IT <andresperezf@yahoo.com>
2017-08-17 23:51:29 -0400

    Handle OBM - Process and show in console.

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 "use strict";
 
-var webServer = require('./lib/webServer');
-var ws = webServer.build();
+const webServer = require('./lib/WebServer');
+const ws = webServer.build();
 
-ws.get('/', function(req, res) {
-	res.send('Application being built. Please wait...');
+const handleOBM = require('./lib/HandleOBM');
+
+ws.get('/', function(reqHTTP, resHTTP) {
+	resHTTP.send('This application handles Outbound Messages sent via a POST request, and therefore does not support GET requests. ' + new Date());
+	resHTTP.end();
+});
+
+ws.post('/', webServer.parseXML, function(reqHTTP, resHTTP, next) {
+	handleOBM.processWS(reqHTTP, resHTTP, null);
 });
