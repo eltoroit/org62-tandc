fc9475e93e8f552dc7b6326356c48ef166e55a3e
ELTORO.IT <andresperezf@yahoo.com>
2017-08-17 20:06:36 -0400

    Remove things that are not required for this app.

--- index.js
+++ index.js
@@ -4,14 +4,5 @@ var webServer = require('./lib/webServer');
 var ws = webServer.build();
 
 ws.get('/', function(req, res) {
-	var params = {};
-	params.dttm = new Date();
-	res.render('helloWorld', params);
-});
-
-ws.post('/', webServer.parseUrlEncoded, function(req, res) {
-	var params = {};
-	params.fname = req.body.fname;
-	params.sfdcStart = req.body.sfdcStart;
-	res.render('helloWorld', params);
+	res.send('Application being built. Please wait...');
 });
