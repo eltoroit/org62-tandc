986edc96e847218787db9c6a8a75426cc481ce77
Your Name <you@example.com>
2018-10-25 15:21:45 -0400

    Added some debugging to the index.js

--- index.js
+++ index.js
@@ -7,10 +7,18 @@ const handleOBM = require('./lib/HandleOBM');
 const sfdc = require('./lib/Salesforce');
 
 ws.get('/', function(reqHTTP, resHTTP) {
+	if (process.env.VERBOSE) {
+		console.log("GET request on root... ignored");
+	}
 	resHTTP.send('This application handles Outbound Messages sent via a POST request, and therefore does not support GET requests. ' + new Date());
 	resHTTP.end();
 });
 
 ws.post('/obm', webServer.parseXML, function(reqHTTP, resHTTP, next) {
+	if (process.env.VERBOSE) {
+		console.log("POST request on /obm... processing");
+		console.log("Body: ");
+		console.log(reqHTTP.body);
+	}
 	handleOBM.processWS(reqHTTP, resHTTP, sfdc);
 });
