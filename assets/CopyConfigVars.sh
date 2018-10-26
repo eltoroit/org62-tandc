c2d8d287c1c71dfa2bf806b2ad40c05b986141b5
Your Name <you@example.com>
2018-10-25 21:52:46 -0400

    text/plain

--- assets/CopyConfigVars.sh
+++ assets/CopyConfigVars.sh
@@ -2,7 +2,7 @@
 
 # ./assets/CopyConfigVars.sh
 FROM=org62-tandc
-TO=org62-tandc-pr-9
+TO=org62-tandc-pr-10
 heroku config -a $FROM --json | jq -M -r '. | keys[] as $k | $k, .[$k]' | \
 while read -r key; read -r val; do
    heroku config:set -a $TO "$key=$val"
